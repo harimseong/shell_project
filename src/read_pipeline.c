81dfcaf12b53a68700884c7bd0c05ad5fc3793d2
Giwoong Son <gson@c2r4s6.42seoul.kr>
2022-06-15 02:54:47 +0900

    fix indent level

--- src/read_pipeline.c
+++ src/read_pipeline.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-# include <stdio.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "libft.h"
