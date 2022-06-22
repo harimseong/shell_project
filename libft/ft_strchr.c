7b0f8c672ae0cb7bfd33d4c58717d95692d64e7f
Harim Seong <hseong@c6r1s1.42seoul.kr>
2022-06-22 12:20:45 +0900

    merge fix

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: hseong <hseong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 20:32:02 by hseong            #+#    #+#             */
-/*   Updated: 2021/11/17 15:57:59 by hseong           ###   ########.fr       */
+/*   Updated: 2022/06/21 17:54:31 by hseong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // different with linux.
 char	*ft_strchr(const char *s, int c)
 {
-	if (c == -1)
+	if (c == -1 || c == 0)
 		return (NULL);
 	while (*s && *s != (char)c)
 		++s;
